c5c41b33946a50797e16b6122038579b30905fbd
David Wolters <d.t.wolters@bradford.ac.uk>
2025-11-14 11:49:46 +0000

    Fixed main initial render

--- src/modules/main/main.ts
+++ src/modules/main/main.ts
@@ -1,11 +1,11 @@
 import h from '@chronocide/hyper';
 
-import { subscribe } from '../../state/store.ts';
+import store, { subscribe } from '../../state/store.ts';
 import board from '../board/board.ts';
 
 import './main.scss';
 
-const main = h('main')()('No board selected');
+const main = h('main')()(store.state.active ? board(store.state.active) : 'No board selected');
 
 subscribe((cur, prev) => prev?.active !== cur.active)(cur => {
   if (typeof cur.active !== 'string') return
